4cdb6d0254aa9bffd0ff180fba9cf52bfa7fd569
Nathaniel Finger <nfinger1020@gmail.com>
2018-05-16 09:42:10 -0400

    bad backslash

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    baseAPI: "https://nate-finger-hangman-server.herokuapp.com/",
+    baseAPI: "https://nate-finger-hangman-server.herokuapp.com",
     googleClientId: "182093567654-ip28bas1niqva90cm8sgkh20btsjacb2.apps.googleusercontent.com",
     googleClientSecret: "LD4buf_nkE6PMU4LnEDSfgUB"
 }
